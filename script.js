98e9bc2a624c5991863b92d5de81e42c25160ae3
Ridwanul Islam <ridwanul161@gmail.com>
2025-02-12 01:42:40 +0600

    dynamic tab

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-const tabs = document.querySelectorAll('.tab_btn');
-let line = document.querySelector('.line');
-
+const tabs = document.querySelectorAll(".tab_btn");
+const all_content = document.querySelectorAll(".tab_content");
+let line = document.querySelector(".line");
 
 tabs.forEach((tab, idx) => {
-    tab.addEventListener('click', (e) => {
-        tabs.forEach(tab => { tab.classList.remove('active') })
-        tab.classList.add('active')
-        line.style.width = e.target.offsetWidth + "px"
-        line.style.left = e.target.offsetLeft + "px"
-    })
-})
+  tab.addEventListener("click", (e) => {
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tab.classList.add("active");
+    line.style.width = e.target.offsetWidth + "px";
+    line.style.left = e.target.offsetLeft + "px";
 
+    all_content.forEach((content) => {
+      content.classList.remove("active");
+    });
+    all_content[idx].classList.add("active");
+  });
+});
