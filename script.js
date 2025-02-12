9a8c59ae9452941d585f40f90f502cc1eb0df7e9
Ridwan4D <ridwanul161@gmail.com>
2025-02-12 12:21:47 +0600

    animation added in header and futura section

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const tabs = document.querySelectorAll(".tab_btn");
 const all_content = document.querySelectorAll(".tab_content");
 let line = document.querySelector(".line");
+const counts = document.querySelectorAll(".futura_bottom_info");
+const speed = 100;
 
 tabs.forEach((tab, idx) => {
   tab.addEventListener("click", (e) => {
@@ -13,7 +15,31 @@ tabs.forEach((tab, idx) => {
 
     all_content.forEach((content) => {
       content.classList.remove("active");
+      setTimeout(upData, 1);
     });
     all_content[idx].classList.add("active");
   });
+});
+
+
+counts.forEach((counter) => {
+  function updateData() {
+    const target = Number(counter.getAttribute("data-target"));
+    let count = 0;
+
+    const increment = target / speed;
+    // console.log(increment)
+
+    const interval = setInterval(() => {
+      count += increment;
+      if (count >= target) {
+        counter.innerText = target + " +";
+        clearInterval(interval);
+      } else {
+        counter.innerText = Math.floor(count) + " +";
+      }
+    }, 8);
+  }
+
+  updateData();
 });
