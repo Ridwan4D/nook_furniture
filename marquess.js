f4e4819aa3abf79ddb41e992139e443104c8e654
Ridwan4D <ridwanul161@gmail.com>
2025-02-13 11:37:21 +0600

    marquee sectioin animation issue fixed

--- marquess.js
+++ marquess.js
@@ -2,11 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const marqueeContainer = document.querySelector(".logos");
   const marqueeSlide = document.querySelector(".logos-slide");
 
-  // Duplicate the content to create an infinite scrolling effect
   marqueeContainer.innerHTML += marqueeSlide.outerHTML;
 
   let currentPosition = 0;
-  const speed = 0.5; // Adjust speed as needed
+  const speed = 10;
 
   function animateMarquee() {
     currentPosition -= speed;
